9884e1040ced7f1363033842bed61a1f4e7ab0f1
Victor Zamora Lopez <victor.zamora@globant.com>
2023-07-20 13:15:41 -0600

    update tf scripts

--- policy-library/iam-binding.tf
+++ policy-library/iam-binding.tf
@@ -1,15 +1,19 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "~> 3.84"
-    }
-  }
+
+resource "google_project_iam_binding" "test_iam_binding" {
+  project = "xtf-validator"
+  role    = "roles/viewer"
+  members = [
+    "user:ixxbit@gmail.com",
+  ]
 }
+
+
+/* 
 resource "google_project_iam_binding" "test_iam_binding" {
   project = "xtf-validator"
   role    = "roles/viewer"
   members = [
     "serviceAccount:policy-validation@xtf-validator.iam.gserviceaccount.com", "user:ixxbit@gmail.com"
   ]
-}
+} 
+*/
