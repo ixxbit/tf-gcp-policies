db13880bccb9dfc53d911f23b7d710514a4429fb
Victor Zamora Lopez <victor.zamora@globant.com>
2023-07-20 12:57:07 -0600

    adding tf scripts

--- policy-library/provider.tf
+++ policy-library/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("../../../xtf-validator-d0dced055346.json")
+  credentials = file("../../xtf-validator-d0dced055346.json")
   project = "xtf-validator"
   region  = "us-central1"
   zone    = "us-central1-c"
